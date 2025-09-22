4fb98f6c210b27be7404a5b446bbc1b0cf6e38c3
ansvver <an5vv3r@outlook.com>
2025-09-22 21:11:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,8 @@ from github import Github
 from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
-MD_HEAD = """## [Gitblog](https://yihong0618.github.io/gitblog/)
-My personal blog([About Me](https://github.com/yihong0618/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
-[Things I like](https://github.com/yihong0618/gitblog/issues/311)
-![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
+MD_HEAD = """## [Gitblog](https://ansvver.github.io/gitblog/)
+![image](https://raw.githubusercontent.com/ansvver/ansvver.github.io.arxiv.2025/refs/heads/master/logo.png)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/main/feed.xml)
 """
 
