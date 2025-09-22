3d466cf7af09847f8cec29002f4fdde6139b10aa
ansvver <an5vv3r@outlook.com>
2025-09-22 20:50:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -296,15 +296,15 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     repo = get_repo(user, repo_name)
     # add to readme one by one, change order here
     add_md_header("README.md", repo_name)
-    # for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
-    #     func(repo, "README.md", me)
+    for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
+        func(repo, "README.md", me)
 
-    #generate_rss_feed(repo, "feed.xml", me)
-    #to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
+    generate_rss_feed(repo, "feed.xml", me)
+    to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
 
     # save md files to backup folder
-    #for issue in to_generate_issues:
-    #    save_issue(issue, me, dir_name)
+    for issue in to_generate_issues:
+        save_issue(issue, me, dir_name)
 
 
 def save_issue(issue, me, dir_name=BACKUP_DIR):
