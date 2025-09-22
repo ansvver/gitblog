57ad5590e5c4b9b73b5b257b8487938ca28933ff
ansvver <an5vv3r@outlook.com>
2025-09-22 19:04:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -299,12 +299,12 @@ def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     # for func in [add_md_firends, add_md_top, add_md_recent, add_md_label, add_md_todo]:
     #     func(repo, "README.md", me)
 
-    generate_rss_feed(repo, "feed.xml", me)
-    to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
+    #generate_rss_feed(repo, "feed.xml", me)
+    #to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
 
     # save md files to backup folder
-    for issue in to_generate_issues:
-        save_issue(issue, me, dir_name)
+    #for issue in to_generate_issues:
+    #    save_issue(issue, me, dir_name)
 
 
 def save_issue(issue, me, dir_name=BACKUP_DIR):
